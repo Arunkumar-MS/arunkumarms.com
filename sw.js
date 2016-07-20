ccc4e5734f093d09db873cac4677c3b59409b007
Arunkumar <arunkumar.shivanna@in.tesco.com>
2016-07-20 23:57:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,45 @@
+var urlsToCache = [
+        '/arunkumarms.com/',
+        '/arunkumarms.com/stylesheets/stylesheet.css',
+        '/arunkumarms.com/stylesheets/github-light.css',
+        '/arunkumarms.com/stylesheets/normalize.css'
+];
+var CACHE_NAME = 'my-site-cache-v2';
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
+this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('CACHE_NAME').then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function() {
+    return caches.match('/sw-test/gallery/myLittleVader.jpg');
+  }));
+});
+
+this.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['v4'];
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
