ae26f36ffb8d04cfe8a13d07db48267870a74308
Arunkumar <arunkumar.shivanna@in.tesco.com>
2016-07-20 23:56:21 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/arunkumarms.com/sw.js', { scope: '/arunkumarms.com/' }).then(function(reg) {
+    
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+    
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+};
