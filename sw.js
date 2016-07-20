c2d78a615b5ccb04c35f0c00b6b884fcf9963692
Arunkumar <arunkumar.shivanna@in.tesco.com>
2016-07-21 00:40:18 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ var urlsToCache = [
         '/arunkumarms.com/stylesheets/github-light.css',
         '/arunkumarms.com/stylesheets/normalize.css'
 ];
-var CACHE_NAME = 'my-site-cache-v4';
+var CACHE_NAME = 'my-site-cache-v5';
 self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
