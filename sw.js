dd2af8d11df3ac53046807c994d4613167692e46
Arunkumar <arunkumar.shivanna@in.tesco.com>
2016-07-21 00:23:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -74,6 +74,7 @@ this.addEventListener('activate', function(event) {
   var cacheWhitelist = ['my-site-cache-v2'];
   event.waitUntil(
     caches.keys().then(function(keyList) {
+            console.log(keyList);
       return Promise.all(keyList.map(function(key) {
         if (cacheWhitelist.indexOf(key) === -1) {
           return caches.delete(key);
