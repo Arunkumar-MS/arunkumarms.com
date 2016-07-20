4ae575e2f58599ee26116edef0d3c5cb5ec89ea2
Arunkumar <arunkumar.shivanna@in.tesco.com>
2016-07-20 23:12:20 +0100

    updated

--- stylesheets/sw.js
+++ stylesheets/sw.js
@@ -1,13 +1,4 @@
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/arunkumarms.com/stylesheets/sw.js',{scope:'/arunkumarms.com/stylesheets/'}).then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-  }).catch(function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
-}
 
 var urlsToCache = [
         '/arunkumarms.com/stylesheets/stylesheet.css',
@@ -84,3 +75,14 @@ this.addEventListener('activate', function(event) {
     })
   );
 });
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/arunkumarms.com/stylesheets/sw.js',{scope:'/arunkumarms.com/stylesheets/'}).then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
