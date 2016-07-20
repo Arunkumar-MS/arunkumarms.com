9a3d59c6770a7ab6358c0d1ef9712e78b0246de7
Arunkumar <arunkumar.shivanna@in.tesco.com>
2016-07-20 23:03:57 +0100

    updated

--- stylesheets/sw.js
+++ stylesheets/sw.js
@@ -1,40 +1,73 @@
+
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/arunkumarms.com/stylesheets/sw.js',{scope:'/arunkumarms.com/stylesheets/'}).then(function(reg) {
-    console.log('◕‿◕', reg);
-  }, function(err) {
-    console.log('ಠ_ಠ', err);
+  navigator.serviceWorker.register('/arunkumarms.com/stylesheets/sw.js',{scope:'/arunkumarms.com/stylesheets/'}).then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
   });
 }
 
-this.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v4').then(function(cache) {
-      return cache.addAll([
+var urlsToCache = [
         '/arunkumarms.com/stylesheets/stylesheet.css',
         '/arunkumarms.com/stylesheets/github-light.css',
         '/arunkumarms.com/stylesheets/normalize.css'
-        
-      ]);
-    })
+];
+var CACHE_NAME = 'my-site-cache-v1';
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-this.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request).then(function(response) {
-        return caches.open('v4').then(function(cache) {
-          console.log(event.request);
-          cache.put(event.request, response.clone());
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
           return response;
-        });  
-      });
-    })
-  );
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have 2 stream.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
 
 this.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['v3'];
+  var cacheWhitelist = ['v4'];
   event.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
