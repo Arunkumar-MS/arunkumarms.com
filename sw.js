a714f8ee453a22f23ce023943e3e727bf694cc5f
Arunkumar <arunkumar.shivanna@in.tesco.com>
2016-07-21 01:25:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,68 +1,41 @@
 var urlsToCache = [
-        '/arunkumarms.com/stylesheets/stylesheet.css',
-        '/arunkumarms.com/stylesheets/github-light.css',
-        '/arunkumarms.com/stylesheets/normalize.css'
+  '/arunkumarms.com/stylesheets/stylesheet.css',
+  '/arunkumarms.com/stylesheets/github-light.css',
+  '/arunkumarms.com/stylesheets/normalize.css'
 ];
-const CACHE_VERSION = 3;
+const CACHE_VERSION = 4;
 let CURRENT_CACHES = {
   assets: 'my-v' + CACHE_VERSION
 };
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   // Perform install steps
   event.waitUntil(
     caches.open(CURRENT_CACHES.assets)
-      .then(function(cache) {
+      .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-/*this.addEventListener('fetch', function(event) {
-  var response;
-  event.respondWith(caches.match(event.request).catch(function() {
-    return fetch(event.request);
-  }).then(function(r) {
-    response = r;
-    caches.open('CACHE_NAME').then(function(cache) {
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }).catch(function() {
-    return caches.match('/sw-test/gallery/myLittleVader.jpg');
-  }));
-});*/
-
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
+      .then(response => {
         if (response) {
           return response;
         }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response
         var fetchRequest = event.request.clone();
-
         return fetch(fetchRequest).then(
-          function(response) {
+          response => {
             // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have 2 stream.
             var responseToCache = response.clone();
 
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
+            caches.open(URRENT_CACHES.assets)
+              .then(cache => {
                 cache.put(event.request, responseToCache);
               });
 
@@ -70,13 +43,13 @@ self.addEventListener('fetch', function(event) {
           }
         );
       })
-    );
+  );
 });
 self.addEventListener('activate', event => {
   // Delete all caches that aren't named in CURRENT_CACHES.
   // While there is only one cache in this example, the same logic will handle the case where
   // there are multiple versioned caches.
-  let expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+  let expectedCacheNames = Object.keys(CURRENT_CACHES).map(key => {
     return CURRENT_CACHES[key];
   });
 
