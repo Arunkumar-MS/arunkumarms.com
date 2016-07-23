87ff9af81d27a75c05683054cb9778716918fe99
Arunkumar <arunkumar.shivanna@in.tesco.com>
2016-07-23 15:02:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,6 +22,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+   console.log(event.request.url);
   event.respondWith(
     caches.match(event.request)
       .then(response => {
